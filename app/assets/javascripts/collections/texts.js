39e39175b215b3fad2b2d162cf8a13331d1ac041
Isaac Murchie <imurchie@gmail.com>
2013-11-14 14:55:57 -0800

    Get rid of some commented-out code

--- app/assets/javascripts/collections/texts.js
+++ app/assets/javascripts/collections/texts.js
@@ -1,13 +1,4 @@
 GooderReads.Collections.Texts = Backbone.Collection.extend({
   model: GooderReads.Models.Text,
-  url: "/texts",
-
-  // parse: function(attrs, options) {
-  //   attrs.forEach(function(attr) {
-  //     var authors = attr.authors;
-  //     attr.authors = new GooderReads.Collections.Authors(authors);
-  //   });
-
-  //   return attrs;
-  // }
+  url: "/texts"
 });
